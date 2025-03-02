4da66b35931ca25e018ef7773b331f48e4640296
techshriyfra <techsashif+eufya@gmail.com>
2025-03-02 23:16:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from bot import Bot
 import pyrogram.utils
+import asyncio
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-Bot().run()
+bot = Bot()
+
+async def main():
+    await bot.start()  # Start bot without use_qr
+    print("Bot is running...")
+    await bot.idle()  # Keep the bot running
+
+asyncio.run(main())
